f95d89ec3b31c74f0d8ee4495f4b94c35410565f
neTp9c <alexpetryakov@gmail.com>
2015-03-18 21:12:35 +0300

    initial commit

---
+++ Extensions/BaseUrlExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Orchard.Environment.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MainBit.Themes.Extensions
+{
+    [OrchardFeature("MainBit.Themes.BaseUrlSelectonRule")]
+    public static class BaseUrlExtensions
+    {
+        public static string GetBaseUrl(this HttpRequestBase request)
+        {
+            return (request.Url.GetLeftPart(UriPartial.Authority) + request.ApplicationPath).TrimEnd('/');
+        }
+    }
+}
---
+++ Services/BaseUrlThemeSelectionRule.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using Orchard;
+using Orchard.Environment.Extensions;
+using Vandelay.Industries.Services;
+using MainBit.Themes.Extensions;
+
+namespace MainBit.Themes.Services {
+    [OrchardFeature("MainBit.Themes.BaseUrlSelectonRule")]
+    public class BaseUrlThemeSelectionRule : IThemeSelectionRule {
+        private readonly IWorkContextAccessor _workContextAccessor;
+
+        public BaseUrlThemeSelectionRule(IWorkContextAccessor workContextAccessor)
+        {
+            _workContextAccessor = workContextAccessor;
+        }
+
+        public bool Matches(string name, string criterion) {
+            var baseUrl = _workContextAccessor.GetContext().HttpContext.Request.GetBaseUrl();
+            return new Regex(criterion, RegexOptions.IgnoreCase)
+                .IsMatch(baseUrl);
+        }
+    }
+}
