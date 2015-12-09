29f3991a14e372cb0b91b14e24d3cb88ac0308bd
neTp9c <alexpetryakov@gmail.com>
2015-12-09 14:29:07 +0300

    add export and import funtions

---
+++ Recipes/Builders/ThemePickerSettingsStep.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+using Orchard.Data;
+using Orchard.Localization;
+using Orchard.Recipes.Services;
+using Vandelay.Industries.Models;
+
+namespace MainBit.Themes.Recipes.Builders {
+    public class ThemePickerSettingsStep : RecipeBuilderStep {
+        private readonly IRepository<ThemePickerSettingsRecord> _repository;
+
+        public ThemePickerSettingsStep(IRepository<ThemePickerSettingsRecord> repository) {
+            _repository = repository;
+        }
+
+        public override string Name {
+            get { return "ThemePickerSettings"; }
+        }
+
+        public override LocalizedString DisplayName {
+            get { return T("Theme Picker Settings"); }
+        }
+
+        public override LocalizedString Description {
+            get { return T("Exports theme picker settings."); }
+        }
+
+        public override void Build(BuildContext context) {
+            var records = _repository.Table.ToList();
+
+            if (!records.Any())
+                return;
+
+            var root = new XElement("ThemePickerSettings");
+            context.RecipeDocument.Element("Orchard").Add(root);
+
+            foreach (var record in records) {
+                root.Add(new XElement("Record",
+                    new XAttribute("Name", record.Name),
+                    new XAttribute("RuleType", record.RuleType),
+                    new XAttribute("Criterion", record.Criterion),
+                    new XAttribute("Theme", record.Theme),
+                    new XAttribute("Priority", record.Priority),
+                    new XAttribute("Zone", record.Zone ?? ""),
+                    new XAttribute("Position", record.Position ?? "")));
+            }
+        }
+    }
+}
+
---
+++ Recipes/Executors/ThemePickerSettingsStep.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Orchard.Data;
+using Orchard.Logging;
+using Orchard.Recipes.Models;
+using Orchard.Recipes.Services;
+using Vandelay.Industries.Models;
+
+namespace MainBit.Themes.Recipes.Executors {
+    public class ThemePickerSettingsStep : RecipeExecutionStep {
+        private readonly IRepository<ThemePickerSettingsRecord> _repository;
+
+        public ThemePickerSettingsStep(
+            IRepository<ThemePickerSettingsRecord> repository,
+            RecipeExecutionLogger logger) : base(logger) {
+            _repository = repository;
+        }
+
+        public override string Name {
+            get { return "ThemePickerSettings"; }
+        }
+
+        public override void Execute(RecipeExecutionContext context) {
+            foreach (var xmlRecord in context.RecipeStep.Step.Elements()) {
+                var name = xmlRecord.Attribute("Name").Value;
+                Logger.Information("Importing theme picker settings '{0}'.", name);
+
+                try {
+                    var record = GetThemePickerSettingsRecord(name);
+                    record.RuleType = xmlRecord.Attribute("RuleType").Value;
+                    record.Criterion = xmlRecord.Attribute("Criterion").Value;
+                    record.Theme = xmlRecord.Attribute("Theme").Value;
+                    record.Priority = int.Parse(xmlRecord.Attribute("Priority").Value);
+                    record.Zone = xmlRecord.Attribute("Zone").Value;
+                    record.Position = xmlRecord.Attribute("Position").Value;
+                    UpdateOrCreateThemePickerSettingsRecord(record);
+                }
+                catch (Exception ex) {
+                    Logger.Error(ex, "Error while importing theme picker settings '{0}'.", name);
+                    throw;
+                }
+            }
+        }
+
+        private ThemePickerSettingsRecord GetThemePickerSettingsRecord(string name) {
+            var record = _repository.Get(x => x.Name == name);
+
+            if (record == null) {
+                record = new ThemePickerSettingsRecord {
+                    Name = name
+                };
+            }
+
+            return record;
+        }
+
+        private void UpdateOrCreateThemePickerSettingsRecord(ThemePickerSettingsRecord record)
+        {
+            if (record.Id > 0)
+                _repository.Update(record);
+            else
+                _repository.Create(record);
+        }
+    }
+}
