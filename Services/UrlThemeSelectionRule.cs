3a1e7ce48425c5c324ba475adb0ac65a4913d4f2
Alex Petryakov <alexpetryakov@gmail.com>
2017-11-26 23:20:14 +0300

    Add url theme selector

---
+++ Services/UrlThemeSelectionRule.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using Orchard;
+using Orchard.Environment.Extensions;
+using Vandelay.Industries.Services;
+using MainBit.Themes.Extensions;
+
+namespace MainBit.Themes.Services {
+    [OrchardFeature("MainBit.Themes.UrlSelectonRule")]
+    public class UrlThemeSelectionRule : IThemeSelectionRule {
+        private readonly IWorkContextAccessor _workContextAccessor;
+
+        public UrlThemeSelectionRule(IWorkContextAccessor workContextAccessor)
+        {
+            _workContextAccessor = workContextAccessor;
+        }
+
+        public bool Matches(string name, string criterion) {
+            var url = _workContextAccessor.GetContext().HttpContext.Request.RawUrl;
+            return new Regex(criterion, RegexOptions.IgnoreCase)
+                .IsMatch(url);
+        }
+    }
+}
